57e5643963758ae1a1bf5d1d5483f6e10422dad3
Geoff Greer <geoff@greer.fm>
2023-05-04 11:35:30 -0700

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+echo "Building website:"
+yarn build
+
+echo "Deploying website:"
+aws s3 sync build/ s3://video.greer.fm/
+
+
+echo "Building docker image:"
+DOCKER_BUILDKIT=1 docker build --ssh default -t server:latest --file server.Dockerfile .
+
+echo "Deploying docker image:"
+aws --region us-west-2 ecr get-login-password | docker login --username AWS --password-stdin 745623752863.dkr.ecr.us-west-2.amazonaws.com/server
+
+docker tag server:latest 745623752863.dkr.ecr.us-west-2.amazonaws.com/server:latest
+docker push 745623752863.dkr.ecr.us-west-2.amazonaws.com/server:latest
+
+aws --region us-west-2 ecs update-service --cluster server --service websocket --force-new-deployment
